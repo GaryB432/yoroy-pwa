69c96174b5de1d8501ecd81d5a14300f13b5e247
Gary Bortosky <garybort@gmail.com>
2018-03-30 16:10:36 -0500

    cleanup

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,20 +1,19 @@
-import { Greeter } from './greeter';
 import { Worder } from './worder';
 
-const greeter: Worder = new Worder('bugler');
+const worder: Worder = new Worder('bugler');
 
 const el = document.getElementById('bugler');
 
 function spinWord() {
-  el!.innerHTML = greeter.create();
+  el!.innerHTML = worder.create();
 }
 
-const up = document.querySelector('.up');
-up!.addEventListener('click', () => {
+const up = document.querySelector('.up')!;
+up.addEventListener('click', () => {
   spinWord();
 });
-const down = document.querySelector('.down');
-down!.addEventListener('click', () => {
+const down = document.querySelector('.down')!;
+down.addEventListener('click', () => {
   spinWord();
 });
 
